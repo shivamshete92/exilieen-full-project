c9b1882737edd8b46e8b9e19f4fc938c9e7a5d90
shivamshete92 <155866429+shivamshete92@users.noreply.github.com>
2025-10-01 15:43:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   "https://exilieen-full-stack-frontend.onrender.com",
   "https://exilieen-tejas-frontend.onrender.com",
-  "http://13.51.160.20"
-];
+  ];
 
 app.use(cors({
   origin: function (origin, callback) {
